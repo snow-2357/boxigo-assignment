d6c69dba42d366b9312d42822a6d275867a493bc
simantaray <simantaray89@gmail.com>
2024-05-25 10:25:30 +0530

    additional section

--- src/components/AdditionalData.jsx
+++ src/components/AdditionalData.jsx
@@ -3,7 +3,8 @@ import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 export default function AdditionalData() {
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 mx-4">
+      <HouseList />
       <Accordion title="Furniture">
         <p>Content for Accordion 1 goes here.</p>
       </Accordion>
@@ -33,7 +34,7 @@ function Accordion({ title, children }) {
   return (
     <div className=" border-gray-300 rounded-lg mb-4">
       <div
-        className="flex justify-between items-center px-4 py-2 cursor-pointer"
+        className="flex justify-between items-center py-2 cursor-pointer"
         onClick={toggleAccordion}
       >
         <div className="bg-gray-200 w-full rounded-sm flex justify-between items-center px-4 py-2">
@@ -47,6 +48,82 @@ function Accordion({ title, children }) {
         </div>
       </div>
       {isOpen && <div className="px-4 py-2  border-gray-300">{children}</div>}
+    </div>
+  );
+}
+
+function HouseList() {
+  return (
+    <div className="w-full border-t">
+      <div className="flex justify-between items-center">
+        <h1 className="text-md font-semibold">Additional Information</h1>
+        <button className="border font-semibold px-2 py-2 bg-black rounded-sm text-white hover:bg-gray-800">
+          Edit Additional Information
+        </button>
+      </div>
+      <p>Test data</p>
+      <div className="flex justify-between items-center">
+        <h1 className="text-md font-semibold">House Details</h1>
+        <button className="border font-semibold px-4 py-2 bg-black rounded-sm text-white hover:bg-gray-800">
+          Edit House Details
+        </button>
+      </div>
+
+      <div className="border-gray-300 py-4 w-full lg:w-2/3">
+        <div>
+          <h2 className="font-semibold text-red-500">Existing House Details</h2>
+          <div className="flex justify-between mt-2">
+            <div>
+              <span className="font-semibold">Floor No</span>
+              <div>
+                <span className="f">3</span>
+              </div>
+            </div>
+            <div>
+              <span className="font-semibold">Elevator Available.</span>
+              <div>
+                <span className="">Yes</span>
+              </div>
+            </div>
+            <div>
+              <span className="font-semibold">
+                Distance from Elevator/Staircase to truck
+              </span>
+              <div>
+                <span className="">1m</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- New House Section --> */}
+      <div className="border-gray-300 py4 mt-4">
+        <div>
+          <h2 className="font-semibold text-red-500">New House Details</h2>
+          <div className="flex justify-between mt-2">
+            <div>
+              <span className="font-semibold">Floor No</span>
+              <div>
+                <span className="f">5</span>
+              </div>
+            </div>
+            <div>
+              <span className="font-semibold">Elevator Available.</span>
+              <div>
+                <span className="">No</span>
+              </div>
+            </div>
+            <div>
+              <span className="font-semibold">
+                Distance from Elevator/Staircase to truck
+              </span>
+              <div>
+                <span className="">2m</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
